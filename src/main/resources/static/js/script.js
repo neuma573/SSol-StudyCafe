5ccfc64b8c1be1f6230ae1897f43ada761660fb7
JiHyun Jang <gihyun0818@gmail.com>
2021-12-06 11:27:40 +0900

    2021-12-06 장지현

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,21 @@
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+ $(document).ready(function(){
+	//nav
+	$('.navTrigger').click(function () {
+	    $(this).toggleClass('active');
+	    console.log("Clicked menu");
+	    $("#mainListDiv").toggleClass("show_list");
+	    $("#mainListDiv").fadeIn();
+	
+	});
+	
+	$(window).scroll(function() {
+	    if ($(document).scrollTop() > 50) {
+	        $('.nav').addClass('affix');
+	        console.log("OK");
+	    } else {
+	        $('.nav').removeClass('affix');
+	    }
+	});
+	 
+ });
 
-});
