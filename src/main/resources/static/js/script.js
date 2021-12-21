3b41bea59d5ca740d7a1eb7412bfe31ada0f6a29
JiHyun Jang <gihyun0818@gmail.com>
2021-12-21 12:25:19 +0900

    2021-12-21 장지현

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,9 @@
+
+var mail_duCheck = false; //이메일 중복 확인
+var pwckcorCheck = false; //비번 확인 일치 확인
+var pwformCheck = false; //비번 형태 확인
+var mailnumCheck = false; //이메일 인증번호 확인
+
 $(document).ready(function() {
 	//nav
 	$('.navTrigger').click(function() {
@@ -378,6 +384,128 @@ $(document).ready(function() {
 		$(this).toggleClass('on').siblings().removeClass('on');
 		$(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
 	});
+
+	//회원가입 
+	if($(".joinCont").length != 0){
+		console.log(1);
+		mail_duCheck = false; //이메일 중복 확인
+		pwckcorCheck = false; //비번 확인 일치 확인
+		pwformCheck = false; //비번 형태 확인
+		mailnumCheck = false; //이메일 인증번호 확인
+		var code = "";
+		$(".auth").click(function() {
+			var email = $('.email_join').val();
+			var checkBox = $(".mail_check_input"); // 인증번호 입력란
+			var boxWrap = $(".mail_check_input_box"); // 인증번호 입력란 박스
+			var checkResult = $("#mail_check_input_box_warn"); // 비교 결과 
+			$.ajax({
+				type: "GET",
+				url: "../mail_du_Check?email=" + email,
+				success: function(data) {
+					console.log("data : " + data);
+					mail_duCheck = data;
+
+					if (mail_duCheck == "1") {
+						checkResult.html("가입 가능한 이메일 입니다.");
+						checkResult.attr("class", "correct");
+						mail_duCheck = true;
+						$.ajax({
+							type: "GET",
+							url: "../mailCheck?email=" + email,
+							success: function(data) {
+								console.log("data : " + data);
+								checkBox.attr("disabled", false);
+								boxWrap.attr("id", "mail_check_input_box_true");
+								code = data;
+							}
+						});
+					} else {
+						checkResult.html("이미 가입된 이메일 입니다.");
+						checkResult.attr("class", "incorrect");
+						mail_duCheck = false;
+
+					}
+
+				}
+			});
+
+			console.log("mail_duCheck : " + mail_duCheck);
+
+		});
+
+		$(".pw_input").blur(function() { //비밀번호 유효성
+			var pw = $('.pw_input').val();
+			var num = pw.search(/[0-9]/g);
+			var eng = pw.search(/[a-z]/ig);
+			var spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+			var checkResult = $("#pw_check_input_box_warn"); // 비교 결과 
+			var pwck = $(".pwck_input"); // 비밀번호 확인 입력란
+			//var boxWrap = $(".mail_check_input_box"); // 인증번호 입력란 박스
+			pw = pw.trim();
+			if (pw.length < 8 || pw.length > 20) {
+
+				checkResult.html("8자리 ~ 20자리 이내로 입력해주세요.");
+				checkResult.attr("class", "incorrect");
+				pwformCheck = false;
+			} else if (pw.search(/\s/) != -1) {
+
+				checkResult.html("비밀번호는 공백 없이 입력해주세요.");
+				checkResult.attr("class", "incorrect");
+				pwformCheck = false;
+			} else if (num < 0 || eng < 0 || spe < 0) {
+				checkResult.html("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
+				checkResult.attr("class", "incorrect");
+				pwformCheck = false;
+			} else {
+				checkResult.html("사용 가능한 비밀번호 입니다.");
+				checkResult.attr("class", "correct");
+				pwck.attr("disabled", false);
+				pwformCheck = true;
+				//boxWrap.attr("id", "mail_check_input_box_true");
+
+			}
+
+			console.log("pwformCheck : " + pwformCheck);
+		});
+
+		//인증번호 비교 
+		$(".mail_check_input").blur(function() {
+			var inputCode = $(".mail_check_input").val(); // 입력코드    
+			var checkResult = $("#mail_check_input_box_warn"); // 비교 결과 
+
+			if (inputCode == code) { // 일치할 경우
+				checkResult.html("인증번호가 일치합니다.");
+				checkResult.attr("class", "correct");
+				mailnumCheck = true;
+			} else { // 일치하지 않을 경우
+				checkResult.html("인증번호를 다시 확인해주세요.");
+				checkResult.attr("class", "incorrect");
+				mailnumCheck = false;
+			}
+
+		
+			console.log("mailnumCheck : " + mailnumCheck);
+		});
+
+		$('.pwck_input').on("propertychange change keyup paste input", function() {
+			var pw = $('.pw_input').val();
+			var pwck = $('.pwck_input').val();
+			if (pw == pwck) {
+				$('.pwck_input_re_1').css('display', 'block');
+				$('.pwck_input_re_2').css('display', 'none');
+				pwckcorCheck = true;
+
+			} else {
+				$('.pwck_input_re_1').css('display', 'none');
+				$('.pwck_input_re_2').css('display', 'block');
+				pwckcorCheck = false;
+			}
+			
+			console.log("pwckcorCheck : " + pwckcorCheck);
+		});
+		
+	}// 회원가입 end 
+
 });
 
 $(window).on('load', function() {
@@ -588,3 +716,24 @@ function liveRev($reserveDetail){
 	$(".reserveCont .totalPay .num").html($reserveDetail.length);
 	$(".reserveCont .totalPay .pay").html(numberWithCommas($seatpayment+$roompayment+$lockerpayment));
 }
+
+
+// 회원가입 
+function Check(mail_duCheck, mailnumCheck, pwckcorCheck, pwformCheck) {
+	console.log(mail_duCheck, mailnumCheck, pwckcorCheck, pwformCheck);
+	if (mail_duCheck) {
+		if (mailnumCheck && pwckcorCheck && pwformCheck) {
+			alert("가입이 완료되었습니다.");
+			return true;
+		} else {
+			alert("인증번호 또는 비밀번호가 틀렸습니다.");
+			return false;
+		}
+	} else {
+		alert("이미 가입된 이메일 입니다.");
+		return false;
+	}
+	return false;
+}
+
+
